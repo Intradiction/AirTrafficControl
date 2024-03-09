b9cf6535a55533cc902b3c5ca5c138a6b21ba265
InnovationTT <greylyngao@gmail.com>
2024-03-09 16:22:05 -0500

    setup GET test operation, FastAPI CORS, test with frontend

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/airplane/{airplane_id}")
+async def ack_airplane(airplane_id):
+    return {"message": f"Airplane with ID={airplane_id}, you are acknowledged and loved"}
