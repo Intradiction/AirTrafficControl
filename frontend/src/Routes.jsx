51f4744aabde37a9c683077d8d66ba2e0ecdc156
Eddy Tran <Eddy.tran453@hotmail.com>
2024-03-25 03:48:14 -0400

    Moved previous app page to test.jsx

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Gates } from './pages/Gates';
+import { Test } from './pages/test';
 
 const Routers = () => {
     return (
         <Router>
             <Routes>
-                <Route exact path="/" element={ <Gates></Gates>}>
-                </Route>
+                <Route exact path="/" element={<Test></Test> }/>
+                <Route exact path="/gates" element={ <Gates></Gates>}/>
+                
             </Routes>
         </Router>
     )
